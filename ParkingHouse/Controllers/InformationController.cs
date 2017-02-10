947aac43c10f6e4b79f7eb405cb35ef5791c0c78
Sir Mimer <sirmimer@hotmail.com>
2017-02-10 08:40:33 +0100

    From school

---
+++ ParkingHouse/Controllers/InformationController.cs
@@ -0,0 +1,23 @@
+﻿using ParkingHouse.DAL;
+using ParkingHouse.Models.ViewModels.Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParkingHouse.Controllers
+{
+    public class InformationController : Controller
+    {
+        private parkingContext db = new parkingContext();
+        // GET: Information
+        public ActionResult Index()
+        {
+            var vehicle = db.Garages.ToList();
+            InformationIndexViewModel model = new InformationIndexViewModel();
+            model = model.toViewModel(vehicle);
+            return View();
+        }
+    }
+}
